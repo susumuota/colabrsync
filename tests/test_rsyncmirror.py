4a355600ef3b309b1933af3bf756b19e056d6bef
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2022-12-13 02:31:32 +0900

    add version test.

--- tests/test_rsyncmirror.py
+++ tests/test_rsyncmirror.py
@@ -2,10 +2,16 @@
 #
 # SPDX-License-Identifier: MIT
 
-from colabrsync import RsyncMirror
+import colabrsync
+from colabrsync import __version__, RsyncMirror
+
+def test_version():
+  assert __version__ == '0.2.0'
 
 def test_rsyncmirror():
   mirror = RsyncMirror('/tmp/src', '/tmp/dst')
   assert mirror.command == 'rsync -aq --log-file /tmp/rsync.log /tmp/src /tmp/dst'
   assert mirror.process
+  assert mirror.process.pid
+  assert mirror.process.returncode == None
   assert mirror.process.args == 'while true ; do rsync -aq --log-file /tmp/rsync.log /tmp/src /tmp/dst ; sleep 60 ; done'
