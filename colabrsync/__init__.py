2f5d95b7aaf1af18bf86705f08ec3d6b9c80725c
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2022-12-13 02:31:59 +0900

    add version.

--- colabrsync/__init__.py
+++ colabrsync/__init__.py
@@ -1,3 +1,7 @@
+from importlib import metadata
 from .rsyncmirror import RsyncMirror
 
-__all__ = ['RsyncMirror']
+__version__ = metadata.version(__package__)
+__all__ = ['__version__', 'RsyncMirror']
+
+del metadata
